157f289462a34ad753d479a3895c46d38f3e2e2f
clara <clara@clara.com>
2017-02-17 15:52:17 -0600

    adding apod request

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var apodRequest = require('../helpers/apod');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,10 +10,29 @@ router.get('/', function(req, res, next) {
 
 /* GET a picture from the APOD service */
 router.get('/fetch_picture', function(req, res, next){
-  if (req.query.today)
-    res.send('To do: fetch today\'s picture');
+
+  if (req.query.today) {
+
+    apodRequest(function(data, error){
+      if (error) {
+        return res.render('apod_error', { error : error.message });
+      }
+      console.log('ROUTE SAYS' + JSON.stringify(data));
+      return res.render('picture', { apod : data });
+    }, true);
+
+  }
   else if (req.query.random) {
-    res.send('To do: fetch random picture');
+
+    apodRequest(function(data, error) {
+      if (error) {
+        return res.render('apod_error', { error : error.message });
+      }
+
+      console.log('ROUTE SAYS' + JSON.stringify(data));
+      return res.render('picture', { apod : data });
+    });
+
   } else {
     next();  // Send to next route handler.
     // Since we haven't defined one, this will end up at the 404 error handler
